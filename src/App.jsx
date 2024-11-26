f5cc11e0b681b4ce501585de2f7f80301ef966ea
jaikrishnan <vjjaikrishnan7890@gmail.com>
2024-11-27 01:48:29 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
 import React, { useEffect } from 'react'
 import Home from './Pages/Home/Home'
+import Player from './Pages/Player/Player';
+import { useLocation } from 'react-router-dom';
 import Login from './Pages/Login/Login'
 import { Routes,Route, useNavigate } from 'react-router-dom'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
 import { ToastContainer, toast } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
-const App = () => {
-  const navigate=useNavigate()
-  useEffect(()=>{
-    onAuthStateChanged(auth,async(user)=>{
-      if(user){
-        console.log('Logged in successfully');
-        navigate('/')
-      }
-      else{
-        console.log('logged out');
-        navigate('/login')
-      }
-    })
-  },[])
+  const App = () => {
+    const navigate = useNavigate();
+    const location = useLocation(); // Get the current route location
+  
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        // Don't redirect if the current location is "/player/:id"
+        if (location.pathname.includes('/player/') || location.pathname === '/login') {
+          return; // Skip the redirect logic for player pages and login page
+        }
+  
+        if (user) {
+          console.log('Logged in successfully');
+          navigate('/'); // Go to Home if logged in
+        } else {
+          console.log('Logged out');
+          navigate('/login'); // Go to Login if logged out
+        }
+      });
+  
+      return () => unsubscribe(); // Cleanup the listener
+    }, [navigate, location]);
+  
+  
   return (
     <div>
       <ToastContainer theme='dark'/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player />} />
       </Routes>
       
     </div>
--- src/Pages/Player/Player.jsx
+++ src/Pages/Player/Player.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png';
+import { useNavigate, useParams } from 'react-router-dom';
 export default function  Player() {
+
+  const {id}=useParams()
+  const navigate=useNavigate()
+
+  const [apiData,setApiData]=useState({
+    name:"",
+    key:"",
+    published_at:"",
+    typeof:""
+  })
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2NiYjE3MDNhY2FmOGMyNjA3OGY1OWYwNjM1NDhmMSIsIm5iZiI6MTczMjY0ODQ4NS4zMjQ2NTIyLCJzdWIiOiI2NzM2ZDRmZWQ2M2ZlZDU4MjZjZjQ0NTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2M9r50OD2PlTd5fw016B22rcbqUzRJLf4Mngj1NY1IE'
+    }
+  };
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+  },[])
+  
+ 
   return (
-    <div className='player' >Player</div>
+    <div className='player' >
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe  width ='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`}  title='trailer' frameBorder='0' allowFullScreen />
+      <div className='player-info'>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
   )
 }
